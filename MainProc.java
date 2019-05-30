b2f538edb33d53ef96a12f1eb7acc70a10bd1e4b
btesija <btesija@combis.hr>
2019-05-30 11:10:29 +0200

    added HelloWorld program..

---
+++ MainProc.java
@@ -0,0 +1,11 @@
+package hr.combis.main;
+
+public class MainProc {
+
+	public static void main(String[] args) {
+		System.out.println("(MainProc.main) !!!! ");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("(MainProc.main) new member -> "+i);
+		}
+	}
+}
